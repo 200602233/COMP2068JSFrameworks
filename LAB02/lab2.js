73d9cc0aadfa3b55ff3f60f08fd03c9b99abeea5
200602233 <200602233@student.georgianc.on.ca>
2026-06-08 19:24:43 -0400

    completed step 3, implementing a prompt to ask user rock, paper or scissors and store answer.

---
+++ LAB02/lab2.js
@@ -0,0 +1,4 @@
+
+//step 3: prompt user to choose rock,paper,or scissors and store the result in a variable called userSelection
+let userSelection = prompt("Rock, Paper, Scissors? Type your choice below: ");
+
