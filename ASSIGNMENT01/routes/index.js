b286385a4847214dade6d3f251c73a8287d94510
200602233 <200602233@student.georgianc.on.ca>
2026-06-13 23:22:58 -0400

    added the hbs to index (i think thats what we do may update later)

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+/* home paeg */
+router.get('/', function(req, res) {
+  res.render('home', { title: 'Home' });
+});
+
+/* about me page */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* projects page */
+router.get('/projects', function(req, res) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* contact paeg */
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
