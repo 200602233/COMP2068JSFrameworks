4616b4c4fa45c53ef025e76f0927628ef54a6e14
200602233 <200602233@student.georgianc.on.ca>
2026-06-13 21:30:21 -0400

    adjusted comments and some math code to follow instructions

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -1,5 +1,5 @@
 
-//step 3: prompt user to choose rock,paper,or scissors and store the result in a variable called userSelection
+// step 3: Use the prompt() npm package to ask the user to choose ROCK, PAPER, or SCISSORS and store their response as userSelection
 const prompt = require("prompt");
 prompt.start();
 
@@ -8,30 +8,42 @@ let userSelection = "";
 // ask user rock paper or scissors
 console.log("\nRock, Paper, or Scissors? ")
 
-// steo 4
+// copilot suggested function
 prompt.get(["userSelection"], function (err, result) {
-    // step 5
     userSelection = result.userSelection.toUpperCase(); // made uppercase for easier comparing
 
-    //step 6 
+   
+    // step 4: Use the Math.random() function to generate a number as computerSelection: 
+    // 0.00 - 0.34 => PAPER
+    // 0.35 - 0.67 => SCISSORS
+    // 0.68 - 1.00 => ROCK
     let randomNum = Math.random()
     let computerSelection;
 
-    //step 7
-    if(randomNum <= 0.34){
+    if(randomNum >= 0.0 || randomNum <= 0.34){
         computerSelection = "PAPER";
     }
-    else if(randomNum <= 0.67){
+    else if(randomNum >= 0.35 || randomNum <= 0.67){
         computerSelection = "SCISSORS";
-    } else{
+    } else if (randomNum >= 0.68 || randomNum <= 1.00){
         computerSelection = "ROCK";
     }
 
-    //step 8
+
+    // step 5: Display both the userSelection and computerSelection variables using console.log()
     console.log("\nYou chose: ", userSelection);
     console.log("Opponent chose: ", computerSelection);
 
-    //step 9
+    
+    // step 6:
+    // Use decision structures to determine who wins the game using regular Rock, Paper, Scissors rules
+    // Use either if or switch
+
+    //step 7:
+    // Display the outcome of the game:
+    // "User Wins"
+    // "Computer Wins"
+    // "It's a tie"
     if (userSelection === computerSelection) {
     console.log("\nIt's a tie\n");
     }
