3ca9d5df22970d39b6f33fdc4fd392487029f2e1
200602233 <200602233@student.georgianc.on.ca>
2026-06-19 06:58:26 -0400

    followed this weeks lesson and implemented the project.js and projects.js

---
+++ ASSIGNMENT01/models/project.js
@@ -0,0 +1,18 @@
+
+
+const mongoose = require("mongoose");
+
+// schema
+const schemaObject = {
+    name: {type: String, required: true},
+    dueDate: { type: Date },
+    status: {
+        type: String,
+        enum: ["TO DO", "IN PROGRESS", "DONE"],
+        default: "TO DO"
+    }
+}
+
+// create model adn explore
+const mongooseSchema = new mongoose.Schema(schemaObject);
+module.exports = mongoose.model("Project", mongooseSchema);
