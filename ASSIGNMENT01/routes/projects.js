2a276c8833bdb397569aa7965119ceab1105b2a9
200602233 <200602233@student.georgianc.on.ca>
2026-06-19 07:06:41 -0400

    weeks 7 example followed and implemented into my assignment

--- ASSIGNMENT01/routes/projects.js
+++ ASSIGNMENT01/routes/projects.js
@@ -9,17 +9,28 @@ const Project = require("../models/project");
 // Configure routes
 // All paths in this file are relative to the configured path in app.js
 // READ - GET /projects > shows a table with all projects in the database
-router.get("/", (req, res, next) => {
-    const projects = 
+router.get("/", async (req, res, next) => {
+    const projects = await Project.find();
     res.render("projects/index", {
         title: "All Projects",
         dataset: projects
-    })
+    });
 });
 // CREATE - GET /projects/new > shows an empty form to create a new project
-
+router.get("/new", (req, res, next) => {
+    res.render("projects/new", {
+        title: "Create a New Project"
+    });
+});
 // CREATE - POST /projects/new > receives from data and creates project in database
-
+router.post("/new", async (req, res, next) => {
+    const newProject = new Project({
+        name: req.body.name,
+        dueDate: req.body.dueDate
+    });
+    await newProject.save();
+    res.redirect("/projects");
+});
 // UPDATE - GET /projects/edit/:id > shows a form with the project data to edit
 
 // UPDATE - POST /projects/edit/:id > receives form data and updates project in database
