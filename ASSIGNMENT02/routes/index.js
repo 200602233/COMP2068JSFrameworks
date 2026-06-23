82e6a33e9cc0936f65a7e46f0cb1bb29ba6b452b
200602233 <200602233@student.georgianc.on.ca>
2026-06-23 18:07:09 -0400

    - npm installed - added hbs files that I think I will need

--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Category page
+router.get('/categories', function(req, res, next) {
+  res.render('categories', { title: 'Express' });
+});
+// Words page
+router.get('/words', function(req, res, next) {
+  res.render('words', { title: 'Express' });
+});
+// phrases apge
+router.get('/phrases', function(req, res, next) {
+  res.render('phrases', { title: 'Express' });
+});
+// adding form page
+router.get('/add', function(req, res, next) {
+  res.render('add-form', { title: 'Express' });
+});
 module.exports = router;
