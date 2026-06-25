850fa1d49307fe1679153186334033a9811e5e22
200602233 <200602233@student.georgianc.on.ca>
2026-06-25 18:29:24 -0400

    added MongoDB connection and installed both dotenv and mongoose

--- ASSIGNMENT02/app.js
+++ ASSIGNMENT02/app.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs = require('hbs');
+
+// load mongoose adn global config
+var mongoose = require('mongoose');
+var configs = require('./configs/globals');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
