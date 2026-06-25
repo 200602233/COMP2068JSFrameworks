6725081d4f170463d389b6076a2171e76b1fda28
200602233 <200602233@student.georgianc.on.ca>
2026-06-25 16:47:26 -0400

    adjusted the renders to have lang implemented so it will change based  on what user selects

--- ASSIGNMENT02/app.js
+++ ASSIGNMENT02/app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// switch language from en to es
+hbs.registerHelper('eq', function (a, b) {
+    return a === b;
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -2,24 +2,72 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// add the const lang so user can choose websites main language
+router.get('/', function (req, res) {
+    const lang = req.query.lang || 'en';
+    let title = 'Home';
+    if (lang === 'es') {
+        title = 'Inicio';
+    }
+    res.render('index', {title: title, lang: lang});
 });
 
 // Category page
 router.get('/categories', function(req, res, next) {
-  res.render('categories', { title: 'Express' });
+  const lang = req.query.lang || 'en';
+  let title = 'Categories';
+  if (lang === 'es') {
+      title = 'Categorías';
+  }
+  res.render('categories', { title: title, lang: lang});
 });
 // Words page
 router.get('/words', function(req, res, next) {
-  res.render('words', { title: 'Express' });
+  const lang = req.query.lang || 'en';
+  let title = 'Words';
+  if (lang === 'es') {
+      title = 'Palabras';
+  }
+  res.render('words', { title: title, lang: lang});
 });
 // phrases apge
 router.get('/phrases', function(req, res, next) {
-  res.render('phrases', { title: 'Express' });
+  const lang = req.query.lang || 'en';
+  let title = 'Phrases';
+  if (lang === 'es') {
+      title = 'Frases';
+  }
+  res.render('phrases', { title: title, lang: lang});
 });
 // adding form page
 router.get('/add', function(req, res, next) {
-  res.render('add-form', { title: 'Express' });
+  const lang = req.query.lang || 'en';
+  let title = 'Add';
+  if (lang === 'es') {
+      title = 'Agregar';
+  }
+  res.render('add', { title: title, lang: lang});
 });
+// all page
+router.get('/all', function(req, res, next) {
+  const lang = req.query.lang || 'en';
+  let title = 'All';
+  if (lang === 'es') {
+      title = 'Todo';
+  }
+  res.render('all', { title: title, lang: lang});
+});
+
+
+// get current URL
+app.use((req, res, next) => {
+
+    res.locals.currentUrl = req.originalUrl;
+    res.locals.lang = req.query.lang || 'en';
+
+    next();
+
+});
+
+
 module.exports = router;
