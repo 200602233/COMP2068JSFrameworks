2397beef7c105a678e239983c128ad9be7e8a889
200602233 <200602233@student.georgianc.on.ca>
2026-06-26 11:58:51 -0400

    added api layout / setup until I find good api to translate within add.hbs

--- ASSIGNMENT02/app.js
+++ ASSIGNMENT02/app.js
@@ -13,6 +13,7 @@ var configs = require('./configs/globals');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ ASSIGNMENT02/routes/api.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) =>{
+    const { text } = req.body;
+
+});
+
+module.exports = router;
